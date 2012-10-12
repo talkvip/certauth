0e5cf44837c16d76b68cc69a8439c7827930a885
kazimsarikaya <kazimsarikaya@sanaldiyar.com>
2012-10-12 12:50:53 +0300

    Several fixes on certificate creation. TLS Server Creation support with Extension CA Extension adding serial key manage

--- src/main/java/com/sanaldiyar/projects/certauth/CAKeyStore.java
+++ src/main/java/com/sanaldiyar/projects/certauth/CAKeyStore.java
@@ -7,10 +7,18 @@ package com.sanaldiyar.projects.certauth;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.security.Key;
 import java.security.KeyPair;
 import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+import java.util.Properties;
+import javax.crypto.spec.SecretKeySpec;
+import sun.security.x509.BasicConstraintsExtension;
+import sun.security.x509.CertificateExtensions;
 import sun.security.x509.X500Name;
 import sun.security.x509.X509CertImpl;
 import sun.security.x509.X509CertInfo;
@@ -20,49 +28,70 @@ import sun.security.x509.X509CertInfo;
  * @author kazim
  */
 public class CAKeyStore {
-    
+
     public final static String CA_PRIVATE_KEY_CERTIFICATE_ALIAS = "CAPriKeyCert";
     public final static int CA_VALID_YEARS = 5;
     public final static int CA_RSA_KEY_BIT_LENGTH = 4096;
+    private final static String KEY_STORE_PATH = "keystore.path";
+    private final static String KEY_STORE_PASSWORD = "keystore.password";
+    private final static String NEXT_SERIAL = "next_serial";
     private String password;
     private File path;
     private KeyStore keyStore;
-    
-    public CAKeyStore(String path, String password) {
-        this.password = password;
+    private long next_serial = 1;
+
+    public CAKeyStore(String path) {
         this.path = new File(path);
     }
-    
+
+    public synchronized long getNextSerial() {
+        long val = next_serial;
+        next_serial++;
+        return val;
+    }
+
     public boolean isLoaded() {
         return keyStore != null;
     }
-    
+
     public void load() throws Exception {
         keyStore = KeyStore.getInstance("JKS");
+        Properties properties = new Properties();
         if (path.exists()) {
+            properties.load(new FileInputStream(path));
+            password = properties.getProperty(KEY_STORE_PASSWORD);
             try {
-                keyStore.load(new FileInputStream(path), password.toCharArray());
+                next_serial = Long.valueOf(properties.getProperty(NEXT_SERIAL));
+                keyStore.load(new FileInputStream(properties.getProperty(KEY_STORE_PATH)), password.toCharArray());
             } catch (Exception ex) {
                 keyStore = null;
                 throw new Exception("Error while loading key store", ex);
             }
         } else {
-            try {
-                keyStore.load(null, null);
-            } catch (Exception ex) {
-                keyStore = null;
-            }
+            properties.setProperty(KEY_STORE_PATH, "key.store");
+            password="123456";
+            properties.setProperty(KEY_STORE_PASSWORD, password);
+            next_serial=1;
+            properties.setProperty(NEXT_SERIAL, Long.toString(next_serial));
+            properties.store(new PrintWriter(path), "");
+            keyStore.load(null, null);
+            keyStore.store(new FileOutputStream("key.store"), password.toCharArray());
+            load();
         }
     }
-    
+
     public void save() throws Exception {
         try {
-            keyStore.store(new FileOutputStream(path), password.toCharArray());
+            Properties properties = new Properties();
+            properties.load(new FileInputStream(path));
+            properties.setProperty(NEXT_SERIAL, Long.toString(next_serial));
+            properties.store(new PrintWriter(path), "");
+            keyStore.store(new FileOutputStream(properties.getProperty(KEY_STORE_PATH)), password.toCharArray());
         } catch (Exception ex) {
             throw new Exception("Error while saving key store", ex);
         }
     }
-    
+
     public void exportCACertificate(String fname) throws Exception {
         if (!isLoaded()) {
             throw new Exception("Not loaded!");
@@ -70,48 +99,64 @@ public class CAKeyStore {
         Certificate certificate = keyStore.getCertificate(CA_PRIVATE_KEY_CERTIFICATE_ALIAS);
         CertUtil.writeCertificateToFile(fname, certificate);
     }
-    
+
     public PrivateKey getCAPrivateKey() throws Exception {
         if (!isLoaded()) {
             throw new Exception("Not loaded!");
         }
         return (PrivateKey) keyStore.getKey(CA_PRIVATE_KEY_CERTIFICATE_ALIAS, password.toCharArray());
     }
-    
+
     public Certificate getCACertificate() throws Exception {
         if (!isLoaded()) {
             throw new Exception("Not loaded!");
         }
         return keyStore.getCertificate(CA_PRIVATE_KEY_CERTIFICATE_ALIAS);
     }
-    
+
     public String getCADN() throws Exception {
         return ((X509CertImpl) getCACertificate()).getIssuerDN().getName();
     }
-    
+
     public X500Name getCAX500Name() throws Exception {
         return new X500Name(((X509CertImpl) getCACertificate()).getIssuerDN().getName());
     }
-    
+
     public void initCA(String dn) throws Exception {
         if (!isLoaded()) {
             throw new Exception("Not loaded!");
         }
         try {
-            
+
             KeyPair caKeyPair = KeyUtil.generateKey(CA_RSA_KEY_BIT_LENGTH);
-            
+
             X500Name caName = new X500Name(dn);
-            
-            X509CertInfo caCertInfo = CertUtil.createCertInfo(caName, caName, CA_VALID_YEARS, caKeyPair.getPublic(), 1);
-            
-            Certificate cert = CertUtil.createAndSignCertificate(caCertInfo, caKeyPair.getPrivate());
-            
+
+            X509CertInfo caCertInfo = CertUtil.createCertInfo(caName, caName,
+                    CA_VALID_YEARS, caKeyPair.getPublic());
+
+            CertificateExtensions extensions = new CertificateExtensions();
+            BasicConstraintsExtension ca = new BasicConstraintsExtension(Boolean.TRUE, Boolean.TRUE, -1);
+            extensions.set("ca", ca);
+
+            caCertInfo.set(CertificateExtensions.NAME, extensions);
+
+            Certificate cert = CertUtil.createAndSignCertificate(caCertInfo,
+                    caKeyPair.getPrivate(), getNextSerial());
+
             keyStore.setKeyEntry(CA_PRIVATE_KEY_CERTIFICATE_ALIAS, caKeyPair.getPrivate(), password.toCharArray(),
                     new Certificate[]{cert});
-            
+
         } catch (Exception ex) {
             throw new Exception("Error while init CA", ex);
-        }        
+        }
+    }
+
+    public void addCertificateToStore(Certificate cert) throws Exception {
+        try {
+            keyStore.setCertificateEntry("cert" + ((X509Certificate) cert).getSerialNumber().toString(), cert);
+        } catch (Exception ex) {
+            throw new Exception("Error adding cert to store", ex);
+        }
     }
 }
--- src/main/java/com/sanaldiyar/projects/certauth/CertUtil.java
+++ src/main/java/com/sanaldiyar/projects/certauth/CertUtil.java
@@ -8,18 +8,25 @@ import java.io.FileOutputStream;
 import java.math.BigInteger;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.cert.CertPath;
 import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
+import java.util.Vector;
+import sun.security.util.ObjectIdentifier;
 import sun.security.x509.AlgorithmId;
 import sun.security.x509.CertificateAlgorithmId;
+import sun.security.x509.CertificateExtensions;
 import sun.security.x509.CertificateIssuerName;
 import sun.security.x509.CertificateSerialNumber;
 import sun.security.x509.CertificateSubjectName;
 import sun.security.x509.CertificateValidity;
 import sun.security.x509.CertificateVersion;
 import sun.security.x509.CertificateX509Key;
+import sun.security.x509.ExtendedKeyUsageExtension;
 import sun.security.x509.X500Name;
 import sun.security.x509.X509CertImpl;
 import sun.security.x509.X509CertInfo;
@@ -30,7 +37,26 @@ import sun.security.x509.X509CertInfo;
  */
 public class CertUtil {
 
-    public static X509CertInfo createCertInfo(X500Name subject, X500Name issuer, int validForYears, PublicKey publicKey, long serial) throws Exception {
+    public static void setAsServerCertificate(X509CertInfo certInfo) throws Exception {
+        try {
+            Vector<ObjectIdentifier> objectIdentifiers = new Vector<ObjectIdentifier>();
+            ObjectIdentifier serverOid = ObjectIdentifier.newInternal(new int[]{1, 3, 6, 1, 5, 5, 7, 3, 1});
+            objectIdentifiers.add(serverOid);
+            ExtendedKeyUsageExtension keyUsageExtension = new ExtendedKeyUsageExtension(Boolean.TRUE, objectIdentifiers);
+
+            CertificateExtensions extensions = (CertificateExtensions) certInfo.get(CertificateExtensions.NAME);
+            if (extensions == null) {
+                extensions = new CertificateExtensions();
+                certInfo.set(CertificateExtensions.NAME, extensions);
+            }
+            extensions.set("serverusage", keyUsageExtension);
+        } catch (Exception ex) {
+            throw new Exception("Error at setting certificate as server certificate", ex);
+        }
+    }
+    
+    public static X509CertInfo createCertInfo(X500Name subject, 
+            X500Name issuer, int validForYears, PublicKey publicKey) throws Exception {
         try {
             Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Europe/Istanbul"));
             Date from = calendar.getTime();
@@ -42,14 +68,11 @@ public class CertUtil {
 
             CertificateValidity certificateValidity = new CertificateValidity(from, to);
             CertificateAlgorithmId certificateAlgorithmId = new CertificateAlgorithmId(algorithmId);
-            CertificateSerialNumber certificateSerialNumber = new CertificateSerialNumber(new BigInteger(Long.toString(serial)));
+            
             CertificateIssuerName certificateIssuerName = new CertificateIssuerName(issuer);
             CertificateSubjectName certificateSubjectName = new CertificateSubjectName(subject);
             CertificateVersion certificateVersion = new CertificateVersion(CertificateVersion.V3);
             CertificateX509Key certificateX509Key = new CertificateX509Key(publicKey);
-
-
-
 
 
             X509CertInfo certInfo = new X509CertInfo();
@@ -57,7 +80,6 @@ public class CertUtil {
             certInfo.set(X509CertInfo.ALGORITHM_ID, certificateAlgorithmId);
             certInfo.set(X509CertInfo.ISSUER, certificateIssuerName);
             certInfo.set(X509CertInfo.KEY, certificateX509Key);
-            certInfo.set(X509CertInfo.SERIAL_NUMBER, certificateSerialNumber);
             certInfo.set(X509CertInfo.SUBJECT, certificateSubjectName);
             certInfo.set(X509CertInfo.VALIDITY, certificateValidity);
             certInfo.set(X509CertInfo.VERSION, certificateVersion);
@@ -68,8 +90,14 @@ public class CertUtil {
         }
     }
 
-    public static Certificate createAndSignCertificate(X509CertInfo certInfo, PrivateKey privateKey) throws Exception {
+    public static Certificate createAndSignCertificate(X509CertInfo certInfo, 
+            PrivateKey privateKey, long serial) throws Exception {
         try {
+            CertificateSerialNumber certificateSerialNumber = 
+                    new CertificateSerialNumber(BigInteger.valueOf(serial));
+            
+            certInfo.set(X509CertInfo.SERIAL_NUMBER, certificateSerialNumber);
+            
             X509CertImpl cert = new X509CertImpl(certInfo);
             cert.sign(privateKey, AlgorithmId.sha1WithRSAEncryption_oid.toString());
 
@@ -84,33 +112,33 @@ public class CertUtil {
         }
     }
 
-//    public static CertPath generateCertificateChain(Certificate ca, Certificate cert) throws Exception {
-//        try {
-//            CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
-//            ArrayList<Certificate> list = new ArrayList<Certificate>();
-//            list.add(ca);
-//            list.add(cert);
-//            CertPath res = certificateFactory.generateCertPath(list);
-//
-//            return res;
-//        } catch (Exception ex) {
-//            throw new Exception("Error while creating cert chain", ex);
-//        }
-//    }
-//
-//    public static void writeCertificateChain(String fname, Certificate ca, Certificate cert) throws Exception {
-//        try {
-//            CertPath chain = generateCertificateChain(ca, cert);
-//            byte[] chain_data = chain.getEncoded("PKCS7");
-//            String s = new String(chain_data);
-//            FileOutputStream fos = new FileOutputStream(fname);
-//            fos.write(chain_data);
-//            fos.close();
-//        } catch (Exception ex) {
-//            throw new Exception("Error at writing cert chain", ex);
-//        }
-//
-//    }
+    public static CertPath generateCertificateChain(Certificate ca, Certificate cert) throws Exception {
+        try {
+            CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+            ArrayList<Certificate> list = new ArrayList<Certificate>();
+            list.add(ca);
+            list.add(cert);
+            CertPath res = certificateFactory.generateCertPath(list);
+
+            return res;
+        } catch (Exception ex) {
+            throw new Exception("Error while creating cert chain", ex);
+        }
+    }
+
+    public static void writeCertificateChain(String fname, Certificate ca, Certificate cert) throws Exception {
+        try {
+            CertPath chain = generateCertificateChain(ca, cert);
+            byte[] chain_data = chain.getEncoded("PKCS7");
+            String s = new String(chain_data);
+            FileOutputStream fos = new FileOutputStream(fname);
+            fos.write(chain_data);
+            fos.close();
+        } catch (Exception ex) {
+            throw new Exception("Error at writing cert chain", ex);
+        }
+
+    }
 
     public static void writeCertificateToFile(String fname, Certificate cert) throws Exception {
         try {
